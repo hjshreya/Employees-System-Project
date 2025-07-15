8cfcff2a2d445d9b968ab6e64ed60d9326f1694a
Shreya J <140332122+hjshreya@users.noreply.github.com>
2025-07-16 00:20:30 +0530

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -2,10 +2,17 @@ class Employee:
     def __init__(self, name, age, salary):
         self.name = name
         self.age = age
-        self.salary = salary
+        self.__salary = salary  # private
+
+    def get_salary(self):
+        return self.__salary
+
+    def set_salary(self, new_salary):
+        if new_salary > 0:
+            self.__salary = new_salary
+        else:
+            raise ValueError("Salary must be positive")
 
     def __str__(self):
-        return f'Employee {self.name} has age {self.age} and salary {self.salary}'
+        return f'Employee {self.name} has age {self.age} and salary {self.__salary}'
 
-    def __repr__(self):
-        return F'Employee(name={self.name}, age={self.age}, salary={self.salary}'
