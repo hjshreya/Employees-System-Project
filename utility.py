2ca82ea2fb4a20f99195ca7697b76de8b3df1fc9
Shreya J <140332122+hjshreya@users.noreply.github.com>
2025-07-16 00:28:55 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,15 +1,15 @@
-def input_is_valid(msg, start=0, end=None):
+def input_is_valid(prompt, start=0, end=None):
     while True:
-        inp = input(msg)
+        try:
+            value = int(input(prompt))
 
-        if not inp.isdecimal():
-            print("Invalid input. Try again!")
-
-        elif start is not None and end is not None:
-            if not (start <= int(inp) <= end):
-                print("Invalid range. Try again!")
+            if end is not None and not (start <= value <= end):
+                print(f"Please enter a number between {start} and {end}.")
+            elif value < start:
+                print(f"Please enter a number greater than or equal to {start}.")
             else:
-                return int(inp)
+                return value
 
-        else:
-            return int(inp)
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
